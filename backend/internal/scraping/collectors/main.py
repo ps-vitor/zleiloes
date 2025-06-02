a9e7e043138dc8cf49b3eee9c914e8dddd83d90b
ps-vitor <the-jungleman@proton.me>
2025-06-02 19:00:23 -0300

    rewrited scraper to golang

--- backend/internal/scraping/collectors/main.py
+++
@@ -1,8 +0,0 @@
-import  time
-from    portalzuk   import  main
-
-if  __name__   ==   "__main__":
-    start=time.time()
-    main()
-    end=time.time()
-    print(f"Tempo total de execução: {end-start:.2f} segundos")
